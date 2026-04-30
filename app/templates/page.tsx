ca48408e044709995fd9342efed0517c265033b8
Ilkin Azimzade <77709522+lknzmzd@users.noreply.github.com>
2026-05-01 01:49:15 +0200

    Create page.tsx

---
+++ app/templates/page.tsx
@@ -0,0 +1,10 @@
+import TemplatePicker from "@/components/TemplatePicker";
+
+export default function TemplatesPage() {
+  return (
+    <main className="page">
+      <h1>Templates</h1>
+      <TemplatePicker onSelect={() => {}} />
+    </main>
+  );
+}
