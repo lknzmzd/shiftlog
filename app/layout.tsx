6aefdbb01c8fe7d0374458a25f9ea1ea0e95c46e
Ilkin Azimzade <77709522+lknzmzd@users.noreply.github.com>
2026-05-01 01:32:15 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,13 @@
-x
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, fontFamily: "sans-serif" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
